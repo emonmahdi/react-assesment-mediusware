a49f52df5fb34406f3a75135e81547a3e5ee18ca
emonmahdi <developeremon1997@gmail.com>
2024-01-29 14:23:28 +0600

    done this assesment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import Problem1 from "./components/Problem1";
+import Problem1 from "./components/Problem1";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Problem2 from "./components/Problem2";
 
@@ -8,8 +8,14 @@ function App() {
       <h3 className="font-bold text-center py-3">
         React Coding Assessment by Mediusware Ltd.
       </h3>
-      {/* <Problem1 /> */}
-      <Problem2 />
+      <Problem1 />
+      <div
+        style={{
+          margin: "40px 0",
+        }}
+      >
+        <Problem2 />
+      </div>
     </>
   );
 }
